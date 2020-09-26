c07e4eed7b51f18e76d4e0fe7dc28095b15910aa
grrlic <18105757+grrlic@users.noreply.github.com>
2020-09-26 14:09:50 +0700

    update magical fruit

--- codeitsuisse/routes/magicfruit.py
+++ codeitsuisse/routes/magicfruit.py
@@ -1,5 +1,6 @@
 import logging
 import json
+import ast
 
 from flask import request, jsonify
 
@@ -9,11 +10,13 @@ logger = logging.getLogger(__name__)
 
 @app.route('/fruitbasket', methods=['POST'])
 def evaluateFruit():
-    data = request.get_json()
+    #data = request.get_json()
+    data = request.get_data()
+    data = data.decode("UTF-8")
+    data = ast.literal_eval(data)
     logging.info("data sent for evaluation {}".format(data))
     result = 0
     for fruit, num in data.items():
         logging.info("fruit, num: {}, {}".format(fruit, num))
         result += num * num * 10
-    #logging.info("My result :{}".format(cheapest))
     return jsonify(result)
